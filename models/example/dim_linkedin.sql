adace2da28bd46c5b06e2527334c3a85342a5d18
rumianatzoneva <133984949+rumianatzoneva@users.noreply.github.com>
2023-09-12 10:03:14 +0200

    Update dim_linkedin.sql

--- models/example/dim_linkedin.sql
+++ models/example/dim_linkedin.sql
@@ -1,17 +1,6 @@
--- with source as (
---     select * from {{source('fivetran_snowflake', 'campaign_history') }}
--- )
+with source as (
+    select * from {{source('fivetran_snowflake', 'campaign_history') }}
+)
 
--- select account_id, format
--- from source
-
--- with source as (
---     select * from {{source('snowflake', 'customer') }}
--- )
-
--- select *
--- from source
-
-select 1
-UNION
-select 2
+select account_id, format
+from source
